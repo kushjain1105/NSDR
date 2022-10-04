018b57096af18b366ecc3cb717d5ee3364588791
kushjain1105 <kushjain1105@gmail.com>
2022-10-04 14:17:25 +0530

    done

--- NSDR/settings.py
+++ NSDR/settings.py
@@ -123,12 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 WHITENOISE_USE_FINDERS = True
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
