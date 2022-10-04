184cbc23e348fe6d6412470ff15ffcbd3ea6cc93
kushjain1105 <kushjain1105@gmail.com>
2022-10-04 11:50:18 +0530

    Added allowed host

--- NSDR/settings.py
+++ NSDR/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$kw!z)(hlego!3^@=7u4u(!wd_r2s0u6*@bi!m$_(%@4=di-=o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'nsdr-webapp.herokuapp.com']
 
 
 # Application definition
