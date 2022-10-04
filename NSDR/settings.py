69c4652a5a1f31f790042df1b6eed32064e02abc
kushjain1105 <kushjain1105@gmail.com>
2022-10-04 13:59:34 +0530

    Added django_heroku

--- NSDR/settings.py
+++ NSDR/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +132,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'Home.User'
+
+django_heroku.settings(locals())
