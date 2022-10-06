9eaec59a08719c436a1a7144e56ccc491b241a24
kushjain1105 <kushjain1105@gmail.com>
2022-10-06 07:35:33 +0530

    Spoiled

--- NSDR/settings.py
+++ NSDR/settings.py
@@ -131,8 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 #Handling Media Files
+MEDIA_ROOT = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
