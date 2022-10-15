f71e02cf2c1960c4bb8866a5d025f8fe2f51b50b
kushjain1105 <kushjain1105@gmail.com>
2022-10-15 19:22:34 +0530

    Removed nsdr_type table

--- Script/models.py
+++ Script/models.py
@@ -2,18 +2,12 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 # Create your models here.
 
-class nsdr_type(models.Model):
-    types = models.CharField(max_length=250)
-
-    def __str__(self):
-        return f"{self.types}"
-    
 class Script(models.Model):
     audio = CloudinaryField("Audio", resource_type="auto")
     name = models.CharField(max_length=250)
     duration = models.DecimalField(max_digits=5, decimal_places=2)
-    nsdr = models.ForeignKey(nsdr_type, on_delete=models.CASCADE)
-
+    nsdr_type = models.CharField(max_length=250)
+    
     def __str__(self):
         return f"{self.name}"
     
