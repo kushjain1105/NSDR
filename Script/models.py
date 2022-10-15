94d3bff174eb4470e55cfff10055a865b8fbf7a4
Kush <kushjain1105@gmail.com>
2022-10-15 19:14:36 +0530

    Changed name of nsdr to nsdrType in Script model

--- Script/models.py
+++ Script/models.py
@@ -12,7 +12,7 @@ class Script(models.Model):
     audio = CloudinaryField("Audio", resource_type="auto")
     name = models.CharField(max_length=250)
     duration = models.DecimalField(max_digits=5, decimal_places=2)
-    nsdr = models.ForeignKey(nsdr_type, on_delete=models.CASCADE)
+    nsdrType = models.ForeignKey(nsdr_type, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name}"
