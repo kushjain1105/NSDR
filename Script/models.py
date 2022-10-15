314e8e14d017b531a6ac7513cf67421c761118cd
kushjain1105 <kushjain1105@gmail.com>
2022-10-15 19:28:09 +0530

    Removed nsdr_type column form Scripts

--- Script/models.py
+++ Script/models.py
@@ -6,7 +6,6 @@ class Script(models.Model):
     audio = CloudinaryField("Audio", resource_type="auto")
     name = models.CharField(max_length=250)
     duration = models.DecimalField(max_digits=5, decimal_places=2)
-    nsdr_type = models.CharField(max_length=250)
     
     def __str__(self):
         return f"{self.name}"
