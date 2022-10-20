4cfb32bf146c193f84910541b7dd5adf69c5f035
Kush <kushjain1105@gmail.com>
2022-10-20 12:45:29 +0530

    Added nsdrType field

--- Script/models.py
+++ Script/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-# Create your models here.    
+# Create your models here.
+class nsdr_type(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+    
 class Script(models.Model):
     audio = CloudinaryField("Audio", resource_type="auto")
     name = models.CharField(max_length=250)
-
+    nsdrType = models.ForeignKey(nsdr_type, on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self):
         return f"{self.name}"    
