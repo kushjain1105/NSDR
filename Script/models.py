28ddc6091ddea82438a90f6582ead87f4c30b3fe
Kush <kushjain1105@gmail.com>
2022-10-20 12:25:58 +0530

    Added Migration

--- Script/models.py
+++ Script/models.py
@@ -12,7 +12,7 @@ class Script(models.Model):
     audio = CloudinaryField("Audio", resource_type="auto")
     name = models.CharField(max_length=250)
     duration = models.DecimalField(max_digits=5, decimal_places=2)
-    nsdrType = models.ForeignKey(nsdr_type, on_delete=models.CASCADE, blank=True, null=True)
+    # nsdrType = models.ForeignKey(nsdr_type, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name}"    
