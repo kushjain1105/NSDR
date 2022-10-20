67aac10e6716c67c7789aaed4a9e906e581a8685
Kush <kushjain1105@gmail.com>
2022-10-20 12:36:51 +0530

    Added things:

--- Script/models.py
+++ Script/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
-class nsdr(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-    
+# Create your models here.    
 class Script(models.Model):
     audio = CloudinaryField("Audio", resource_type="auto")
     name = models.CharField(max_length=250)
-    duration = models.DecimalField(max_digits=5, decimal_places=2)
-    # nsdrType = models.ForeignKey(nsdr_type, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name}"    
