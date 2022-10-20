ea4226608678aed11b54c80129a8fe3d976fd545
kushjain1105 <kushjain1105@gmail.com>
2022-10-20 09:55:46 +0530

    Added sorting Algorithm for sctips

--- Script/views.py
+++ Script/views.py
@@ -3,8 +3,20 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import *
 # Create your views here.
-def index(request):
+def sortScripts():
     scripts = Script.objects.all()
+    scriptNames = []
+    for script in scripts:
+        scriptNames.append(script.name)
+    scriptNames.sort()
+
+    scriptObjects = []
+    for script in scriptNames:
+        scriptObjects.append(Script.objects.get(name=scriptNames))
+    return scriptObjects
+
+def index(request):
+    scripts = sortScripts()
     
     return render(request, "Script/index.html", {
         "scripts": scripts
