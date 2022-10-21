190e14a1a05f7b8f1bd9145d48671f7cde2dcd58
kushjain1105 <kushjain1105@gmail.com>
2022-10-21 18:23:55 +0530

    Changed sorting algorithm

--- Script/views.py
+++ Script/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import *
+
 # Create your views here.
 def sortScripts():
     scripts = Script.objects.all()
     scriptNames = []
+    counter = 0
     for script in scripts:
+        counter += 1
+        if counter > 9:
+            break
+        
         scriptNames.append(script.name)
     scriptNames.sort()
 
