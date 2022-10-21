2096ad1536b3e73f9569bad64bae5fb4dce1e225
kushjain1105 <kushjain1105@gmail.com>
2022-10-21 13:43:30 +0530

    Added a lot of stuff

--- Research/models.py
+++ Research/models.py
@@ -6,6 +6,7 @@ class Project(models.Model):
     # Add date to project Field
     title = models.CharField(max_length=150)
     members = models.ManyToManyField(Member, related_name="projects")
+    date = models.DateField(default=datetime.datetime.today())
 
     def __str__(self):
         return f"Research Project: {self.title}"
@@ -14,7 +15,7 @@ class Paper(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    date = models.DateField(default=datetime.datetime.today())
+
     def __str__(self):
         return f"Research Paper: {self.title}"
 
