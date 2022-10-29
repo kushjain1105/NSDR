7aae86606c83245a6dfa89b024db1c1aad8d2b69
Kush <kushjain1105@gmail.com>
2022-10-29 12:07:21 +0530

    Added Reports Model to Dashboard App

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from Home.models import User
 # Create your models here.
+class Report(models.Model):
+    subject = models.ForeignKey(User, on_delete=models.CASCADE)
+    graphURL = models.URLField()
+    dateTime = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.subject}'s Report"
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def sortReport(report):
+    return report.dateTime
 def index(request):
-    return render(request, "Dashboard/index.html")
+    reportObjects = Report.objects.filter(subject=request.user)
+    reports = []
+    for report in reportObjects:
+        reports.append(report)
+    reports.sort(key=sortReport, reverse=True)
+    report = reports[0]
+    return render(request, "Dashboard/index.html", {
+        "report": report
+    })
 
 def previous_reports(request):
-    return render(request, "Dashboard/previous_reports.html")
+    reportObjects = Report.objects.filter(subject=request.user)
+    reports = []
+    for report in reportObjects:
+        reports.append(report)
+    reports.sort(key=sortReport, reverse=True)
+    previousReports = reports[1:]
+    return render(request, "Dashboard/previous_reports.html", {
+        "previousReports": previousReports
+    })
