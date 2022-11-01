e5f30b924ae5c4167315ff976cd5e7bf52830b2c
Kush <kushjain1105@gmail.com>
2022-11-01 12:12:29 +0530

    Added Excel Charts

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -3,7 +3,7 @@ from Home.models import User
 # Create your models here.
 class Report(models.Model):
     subject = models.ForeignKey(User, on_delete=models.CASCADE)
-    graphURL = models.URLField()
+    graphURL = models.CharField(max_length=500)
     dateTime = models.DateTimeField()
 
     def __str__(self):
