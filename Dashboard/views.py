c097287ce25b7a108821fb237a294d11abb0ee85
Kush <kushjain1105@gmail.com>
2022-11-03 09:20:10 +0530

    Added Logic to Dashboard

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -3,15 +3,22 @@ from .models import *
 # Create your views here.
 def sortReport(report):
     return report.dateTime
+
 def index(request):
     reportObjects = Report.objects.filter(subject=request.user)
     reports = []
     for report in reportObjects:
         reports.append(report)
     reports.sort(key=sortReport, reverse=True)
-    report = reports[0]
+    if len(reports) != 0:
+        report = reports[0]
+    else:
+        report = None
+        message = "No Reports yet!"
+        
     return render(request, "Dashboard/index.html", {
-        "report": report
+        "report": report,
+        "message": message
     })
 
 def previous_reports(request):
